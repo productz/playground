f0ad867f385f756fe26be16d1161485e69e5c81d
Osama Alghanmi <osamaalghanmi@Osamas-Mac-mini.local>
2017-01-29 14:34:34 -0700

    adding some configuration files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+'use strict';
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + '/src', // `__dirname` is root of project and `src` is source
+    entry: {
+        app: './src/javascript/index.js',
+    },
+    output: {
+        path: __dirname + '/dist', // `dist` is the destination
+        filename: 'bundle.js',
+        publicPath: "/assets",
+    },
+    module: {
+        rules: [
+        {
+            test: /\.js$/, //Check for all js files
+            use: [{
+                loader: 'babel-loader',
+                options: { presets: ['es2015'] }
+            }]
+        },
+        {
+            test: /\.(sass|scss)$/, //Check for sass or scss file names
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+            ]
+        },
+        { 
+            test: /\.json$/, 
+            loader: "json-loader"  //JSON loader
+        }
+        ]
+    },
+    //To run development server
+    devServer: {
+        contentBase: __dirname + '/src',
+    },
+};
