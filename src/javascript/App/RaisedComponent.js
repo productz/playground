709ea3c12876e788842cd5ac69f4e913890bbaa8
Osama Alghanmi <osamaalghanmi@Osamas-Mac-mini.local>
2017-01-29 15:30:22 -0700

    adding some components

---
+++ src/javascript/App/RaisedComponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const RaisedComponent = () => (
+        <RaisedButton label="Default" />
+        );
+
+export default RaisedComponent;
