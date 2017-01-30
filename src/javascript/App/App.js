2c706384107acd04d8e50615ec543b6838ba0649
Sam Alghanmi <oalghnmi@gmail.com>
2017-01-30 02:26:11 +0000

    plugins

--- src/javascript/App/App.js
+++ src/javascript/App/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedComponent from './RaisedComponent';
+import AppBarExampleComposition from '../components/AppBar/ExampleComposition.js'
 
 const App = () => (
         <MuiThemeProvider>
         <RaisedComponent />
+        <AppBarExampleComposition />
         </MuiThemeProvider>
         );
 
