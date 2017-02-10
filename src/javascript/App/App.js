f62f0d3ab8bbe1d4d9a67fd4f35f4e8c3318be53
Sam Alghanmi <oalghnmi@gmail.com>
2017-02-10 22:23:16 +0000

    added gridlist

--- src/javascript/App/App.js
+++ src/javascript/App/App.js
@@ -48,6 +48,27 @@ class App extends React.Component{
     }
 };
 
+/**
+ * This example demonstrates the horizontal scrollable single-line grid list of images.
+ */
+const GridListExampleSingleLine = (tilesData) => (
+  <div style={styles.root}>
+    <GridList style={styles.gridList} cols={2.2}>
+      {tilesData.map((tile) => (
+        <GridTile
+          key={tile.img}
+          title={tile.title}
+          actionIcon={<IconButton><StarBorder color="rgb(0, 188, 212)" /></IconButton>}
+          titleStyle={styles.titleStyle}
+          titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+        >
+          <img src={tile.img} />
+        </GridTile>
+      ))}
+    </GridList>
+  </div>
+);
+
 const CardFrame = ({children}) => (
         <Card>
         <CardHeader
