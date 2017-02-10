b7b1f96103f5eab919336768712b8a1bfb9a99ef
Sam Alghanmi <oalghnmi@gmail.com>
2017-02-10 10:32:28 -0700

    added build

--- Gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-
-gulp.task('default', function(done){
-});
-gulp.task('presentation', function(done){
-});
-gulp.task('mindmap:save', function(done){
-});
