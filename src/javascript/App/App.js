d96ceb8270ce2f89514484a29a4f5c4cb0452fcd
Sam Alghanmi <oalghnmi@gmail.com>
2017-02-18 20:30:00 +0000

    added portfolio and styled portfolio and about pages

--- src/javascript/App/App.js
+++ src/javascript/App/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Router, Route, IndexRoute, Link, hashHistory, browserHistory } from 'react-router'
-import {Tabs, Tab} from 'material-ui/Tabs';
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import {
+    Router,
+    Route,
+    IndexRoute,
+    Link,
+    hashHistory
+}
+from 'react-router'
+import {
+    Tabs,
+    Tab
+}
+from 'material-ui/Tabs';
+import {
+    Card,
+    CardHeader,
+    CardText
+}
+from 'material-ui/Card';
 import AppBar from 'material-ui/AppBar';
 import FontIcon from 'material-ui/FontIcon';
 import MapsPersonPin from 'material-ui/svg-icons/maps/person-pin';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import 'normalize.css';
+import '../Style/main.scss';
 import data from '../data.json';
 
 
@@ -20,21 +38,29 @@ const styles = {
     root: {
         display: 'flex',
         flexWrap: 'wrap',
+        flexDirection: 'column',
         justifyContent: 'space-around',
     },
     gridList: {
         display: 'flex',
         flexWrap: 'nowrap',
+        flexDirection: 'row',
         overflowX: 'auto',
+    },
+    gridTitle: {
+        textDecoration: 'underline'
+    },
+    gridItem: {
+        padding: '0 3em'
     },
     titleStyle: {
         color: 'rgb(0, 188, 212)',
     },
 };
 
-class App extends React.Component{
-    render(){
-        return(
+class App extends React.Component {
+    render() {
+        return (
             <MuiThemeProvider>
                 <div>
                     <AppBar
@@ -63,8 +89,10 @@ class App extends React.Component{
     }
 };
 
-const CardFrame = ({children}) => (
-        <Card>
+const CardFrame = ({
+    children
+}) => (
+    <Card>
         <CardHeader
         />
         <CardText >
@@ -73,8 +101,10 @@ const CardFrame = ({children}) => (
         </Card>
 );
 
-const Menu = ({changeRoute}) => (
-        <Tabs>
+const Menu = ({
+    changeRoute
+}) => (
+    <Tabs>
         <Tab
         icon={<FontIcon className="material-icons">home</FontIcon>}
         label="Home"
@@ -108,69 +138,88 @@ const Footer = () => (
 );
 
 const Home = () => (
-        <article>
+    <article>
             <h3>Welcome to my personal Site.</h3> 
             <h3>My name is Sam Alghanmi. I am a full-stack developer. My main language is Javascript.</h3>
         </article>
 );
 
 const cleanObject = (obj) => {
-    return Object.keys(obj).map((key)=>{
-        if(parseInt(key)){
+    return Object.keys(obj).map((key) => {
+        if (parseInt(key)) {
             return obj[key];
         }
         return "";
-    })
+    }).filter((obj)=>obj !== "")
 }
 
 const Skills = () => {
-        let skill = data.ideas.About.ideas["My Skills"].ideas;
-        let skillList = [
-            {category:"Frontend", ideas: skill["Web"].ideas["Frontend"].ideas},
-            {category:"Backend", ideas: skill["Web"].ideas["Backend"].ideas},
-            {category: "Hybrid Mobile", ideas: skill["Mobile"].ideas["Hybrid"].ideas},
-            {category: "On the Side", ideas:skill["On the Side"].ideas}
-        ];
-        return (<div style={styles.root}>
-            <div style={styles.gridList} cols={2.2}>
+    let skill = data.ideas.About.ideas["My Skills"].ideas;
+    let skillList = [{
+        category: "Frontend",
+        ideas: skill["Web"].ideas["Front-end"].ideas
+    }, {
+        category: "Backend",
+        ideas: skill["Web"].ideas["Backend"].ideas
+    }, {
+        category: "Hybrid Mobile",
+        ideas: skill["Mobile"].ideas["Hybrid"].ideas
+    }, {
+        category: "On the Side",
+        ideas: skill["On the Side"].ideas
+    }];
+    return (<div style={styles.root} >
               {skillList.map((skill,index) => (
               <div>
-                <h2>
+                <h2 style={styles.gridTitle}>
                 {skill.category}
                 </h2>
-                <div key={index}>
+                <div style={styles.gridList} key={index}>
                     {cleanObject(skill.ideas).map((subSkill)=>(
-                        subSkill.title
+                        <span style={styles.gridItem}>{subSkill.title}</span>
                     ))}
                 </div>
-                </div>
+               </div>
               ))}
-            </div>
-          </div>);   
+            </div>);
 }
 
 const About = () => {
-        return (<article>
-            <h1>About Me</h1>
-            <h2>
+    return (<article>
+            <h3>
             I am full stack developer with 7 years of professional experience. My main stack is composed of JavaScript: React, Angularjs, Redux, Mobx, RxJS, Webpack are some of what I know on the front-end. On the back-end, I am familiar with Node: Express, PostgreSQL on the back-end. I have been focusing my efforts in the last two years on working with Hybrid Mobile apps in React Native. 
             I have worked in most settings of software development. I have run my own agency, worked at an agency, in-house on a product team, and freelanced. My diverse background empowers me to bring a comprehensive view into any technical issue I am faced with.
-            </h2>
+            </h3>
             <h2>Skills:</h2>
-            <div className="grid">
-                <i className="devicon-amazonwebservices-original"></i>
-                <i className="devicon-php-plain"></i> 
-            </div>
             {Skills()}
         </article>);
 };
 
+const dialog = ({
+    text
+}) => (
+    <div>howdy</div>
+)
+
+const Companies = () => {
+    let companyList = data.ideas.Portfolio.ideas;
+    return (<div style={styles.root} >
+              {cleanObject(companyList).map((company,index) => (
+              <div>
+                <h2 style={styles.gridTitle}>
+                {company.title}
+                </h2>
+                <h3>{company.attr.note.text}</h3>
+               </div>
+              ))}
+            </div>);
+}
+
 const Portfolio = () => (
-    <article>
-        <h3>
-        Portfolio
-        </h3>
-    </article>
+    <div>
+        <h3>Companies I worked With: </h3>
+        {Companies()}
+    </div>
 );
 
 const Contact = () => (
@@ -182,7 +231,7 @@ const Contact = () => (
 );
 
 ReactDOM.render(
-        <Router history={hashHistory}>
+    <Router history={hashHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={Home} />
             <Route path="about" component={About} />
@@ -190,5 +239,5 @@ ReactDOM.render(
             <Route path="contact" component={Contact} />
         </Route>
         </Router>,
-        document.getElementById('app')
-        );
+    document.getElementById('app')
+);
