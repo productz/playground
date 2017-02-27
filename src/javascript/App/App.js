0c511fa52a02cd094224d3fc38de2a6fd934ef8d
Sam Alghanmi <oalghnmi@gmail.com>
2017-02-27 03:14:03 +0000

    app.js and styles

--- src/javascript/App/App.js
+++ src/javascript/App/App.js
@@ -241,11 +241,11 @@ const Skills = () => {
 
 const About = () => {
     return (<section>
-            <h3>
+            <p>
             I am full stack developer with 7 years of professional experience. My main stack is composed of JavaScript: React, Angularjs, Redux, Mobx, RxJS, Webpack are some of what I know on the front-end. On the back-end, I am familiar with Node: Express, PostgreSQL on the back-end. I have been focusing my efforts in the last two years on working with Hybrid Mobile apps in React Native. 
             I have worked in most settings of software development. I have run my own agency, worked at an agency, in-house on a product team, and freelanced. My diverse background empowers me to bring a comprehensive view into any technical issue I am faced with.
-            </h3>
-            <h2>Skills:</h2>
+            </p>
+            <h3 className="title">Skills:</h3>
             {Skills()}
         </section>);
 };
