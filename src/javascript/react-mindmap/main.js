bdea70d597bb859584c2043a6c3d791d96d65a75
Sam Alghanmi <oalghnmi@gmail.com>
2017-03-02 17:11:54 +0000

    react mimdmap

---
+++ src/javascript/react-mindmap/main.js
@@ -0,0 +1,7 @@
+//@flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App.js';
+const render = () => ReactDOM.render(<App/>
+                ,document.getElementById('app'));
+render();
