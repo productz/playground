daa9a32d609d23ce79724272e0b9d3191bc77d3b
Sam Alghanmi <oalghnmi@gmail.com>
2017-03-17 02:29:26 +0000

    added olded rendering

--- src/javascript/Model/Model.js
+++ src/javascript/Model/Model.js
@@ -21,8 +21,99 @@ export function calculateInitialPositions(mindmap,parent) {
 }
 
 export function calculatePositions(mindmap, parent) {
+    //central idea
 }
 
-export function updatePosition(idea) {
+//=============================
+//========== Old Rendering
+//=============================
 
+function render(tree) {
+
+    this.tree = tree;
+    var x, y;
+    var length;
+    var mainContainer;
+    var box;
+    var sText;
+    var text;
+    var sibling;
+    var siblingHeight = 0;
+    var arrangement;
+    var debugRect;
+    this.currentHeight = 0;
+    var gPosition = {
+        y: 0
+    };
+    var line;
+    var that = this;
+    traverse(tree, (mindmapObj, key, parent) => {
+
+        box = this.createBox();
+        box.interactive = true;
+        mainContainer = new PIXI.Container();
+        debugRect = new PIXI.Graphics();
+
+        mindmapObj.mainContainer = mainContainer;
+        mindmapObj.box = box;
+
+        sText = mindmapObj.title.slice(0, 10);
+        text = this.createText(sText);
+        if (text.width > box.width) {
+            box.width = text.width;
+        }
+        else {
+            text.y += 10
+        }
+        //store a reference to the object here to be used when updating the object's position
+        box.obj = mindmapObj;
+        box.addChild(text);
+        mainContainer.addChild(box);
+
+        // events for drag start
+        box
+            .on('mousedown', onDragStart)
+            .on('touchstart', onDragStart)
+            // events for drag end
+            .on('mouseup', onDragEnd)
+            .on('mouseupoutside', onDragEnd)
+            .on('touchend', onDragEnd)
+            .on('touchendoutside', onDragEnd)
+            // events for drag move
+            .on('mousemove', onDragMove)
+            .on('touchmove', onDragMove);
+
+        if (parent) {
+            //we need to add an offset on both sides to cover when we move the elements up and down
+            //maybe we can move the bounds out a little?
+            parent.mainContainer.addChild(mainContainer);
+            //get previous sibling
+            var count = 0;
+            var order;
+            parent.ideasArr = [];
+            Object.keys(parent.ideas).map((key, index) => {
+                parent.ideasArr.push(parent.ideas[key]);
+                count++;
+            })
+            sibling = parent.ideasArr[mindmapObj.order - 1];
+            length = count;
+            if (sibling) {
+                mainContainer.x += HORIZONTAL_MARGIN;
+                mainContainer.y = sibling.mainContainer.y + sibling.mainContainer.height + VERTICAL_MARGIN;
+            }
+            else {
+                mainContainer.x += HORIZONTAL_MARGIN;
+                //mainContainer.y -= ((-1 * Math.floor(length/2)) + mindmapObj.order);
+            }
+            //for debugging
+            if (mindmapObj.title.indexOf("Concepts") !== -1) {}
+            //drawLine
+            var po = box.toLocal(mainContainer.position);
+            this.drawLine(mainContainer, parent.mainContainer, box, parent.box);
+            mindmapObj.line = line;
+        }
+        else {
+            this.rootContainer.addChild(mainContainer);
+        }
+    })
 }
