c2b5bec797cec05dba79ae1a1764443bf332e776
Sam Alghanmi <oalghnmi@gmail.com>
2017-03-19 13:10:57 +0000

    trying it out with more

--- src/javascript/App/App.js
+++ src/javascript/App/App.js
@@ -2,7 +2,7 @@
 import Tree from '../Components/Tree.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import mindmap from '../data.json';
+import mindmap from '../../../data/JavaScript.mup.json';
 import Model from '../Model/Model.js';
 
 export default class App extends React.Component {
