843636b7e9336535f24a7217b08a0c5e66e73852
Sam Alghanmi <oalghnmi@gmail.com>
2017-05-01 17:28:37 -0400

    shuffling

--- src/node/graphql-service/schema/index.js
+++
@@ -1,9 +0,0 @@
-import { GraphQLSchema, GraphQLObjectType } from 'graphql';
-import { aws } from './aws';
-
-const schema = new GraphQLSchema({
-  mutation: aws.Mutations,
-  query: aws.Queries
-});
-
-export default schema;
--- src/node/server.js
+++ src/node/server.js
@@ -1,61 +1,33 @@
-// =================================================================
-// get the packages we need ========================================
-// =================================================================
-var express 	= require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
-var mongoose    = require('mongoose');
-var config = require('config'); // get our config file
+import getRandom from './utils.js';
 
-// =================================================================
-// configuration ===================================================
-// =================================================================
-var port = config.get('server.port'); // used to create, sign, and verify tokens
-var ip = config.get('server.ip');
-//mongoose.connect(`${config.get('db.host')}:${config.get('db.port')}`); // connect to database
-app.set('superSecret', config.secret); // secret variable
+class Deck{
+  cardList = [
+    new Card("jack","spade"),
+    new Card("queen","hearts"),
+    new Card("1","spade")
+  ];
+  constructor(){
 
-// =================================================================
-// Import web services ========================================
-// =================================================================
-var User   = require('./db-service/user'); // get our mongoose model
+  }
+  //number: represents the number of shuffles we are going to perform each time
+  shuffle(number){
+    for(let i=0;i<number;i++){
+      let startIndex = getRandom(1,52);
+      let endIndex = getRandom(startIndex,52);
+      let removed = this.cardList.splice(startIndex,endIndex);
+      this.cardList.unshift(removed);
+    }
+  }
+  deal(){
+    return this.cardList[this.cardList.length - 1];
+  }
+}
 
-import authService from './auth-service/auth-service.js'
-const authApi = authService({app,User});
-
-import helloService from './hello-service/hello-service.js'
-const helloApi = helloService({app,User});
-
-import dbService from './db-service/db-service.js'
-const dbApi = dbService({app});
-
-import passportService from './passport-service/passport-service.js'
-const passportApi = passportService({app,User,config});
-
-import socketService from './socket-service/socket-service.js'
-const socketApi = socketService({app});
-
-// use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// use morgan to log requests to the console
-app.use(morgan('dev'));
-
-// ==========
-// Register Services
-// ==========
-
-//app.use('/', authApi);
-app.use('/hello',helloApi);
-//app.use('/db',dbApi);
-//app.use('/',passportApi);
-
-app.use('/socket-io',socketApi);
-
-// =================================================================
-// start the server ================================================
-// =================================================================
-app.listen(port,ip);
-console.log('Magic happens at http://localhost:' + port);
+class Card{
+  name;
+  type;
+  constructor(name,type){
+    this.name = name;
+    this.type = type;
+  }
+}
