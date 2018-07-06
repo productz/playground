9cc9294c253ae10ec91d67d4977c097b9b610968
salghanmi <salghanmi@touchtown.us>
2018-07-05 21:29:43 -0400

    card design

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import { connect } from "react-redux";
 import { FETCH_DATA } from "../../types";
 import "./App.css";
 import { Grid, Col, Row } from "react-styled-flexboxgrid";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../../theme";
 import { IMAGE_API } from "../../constants";
 
 const mapStateToProps = state => {
@@ -28,22 +29,32 @@ class App extends Component {
   render() {
     let { artists } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Dillon Sample Project (by Sam Alghanmi)</h1>
-        </header>
-        <Grid>
-          <Row>
-            {artists.map(artist => (
-              <Col xs={6} md={3}>
-                <img src={IMAGE_API(artist.artKey)} />
-                <h1>{artist.title}</h1>
-                <h2>{artist.artistName}</h2>
-              </Col>
-            ))}
-          </Row>
-        </Grid>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">
+              Dillon Sample Project (by Sam Alghanmi)
+            </h1>
+          </header>
+          <Grid>
+            <Row>
+              {artists.map(artist => (
+                <Col xs={6} md={3}>
+                  <div className="card">
+                    <div className="card__image__container">
+                      <img src={IMAGE_API(artist.artKey)} />
+                    </div>
+                    <div className="card__description">
+                      <p className="card__title">{artist.title}</p>
+                      <p className="card__artist"><i>{artist.artistName}</i></p>
+                    </div>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </Grid>
+        </div>
+      </ThemeProvider>
     );
   }
 }
--- src/theme.js
+++ src/theme.js
@@ -1,29 +1,29 @@
-import {injectGlobal} from 'styled-components'
+import { injectGlobal } from "styled-components";
 
 const colors = {
-  primary: '#0eb1d2',
-  accent: '#02182b',
-  error: '#d7263d',
-  contrast: '#dee5e5'
-}
+  primary: "#0eb1d2",
+  accent: "#02182b",
+  error: "#d7263d",
+  contrast: "#dee5e5"
+};
 
 const margins = {
-  bottom: '1.5rem'
-}
+  bottom: "1.5rem"
+};
 
 const paddings = {
-  quarter: '0.25rem',
-  half: '0.5rem',
-  base: '1rem',
-  double: '2rem'
-}
+  quarter: "0.25rem",
+  half: "0.5rem",
+  base: "1rem",
+  double: "2rem"
+};
 
 // Reusable definitions for colors, spacings, etc.
 export const theme = {
   colors,
   margins,
   paddings
-}
+};
 
 // Inject some global styles that are most likely to be coupled to theme variables.
 injectGlobal`
@@ -33,8 +33,36 @@ injectGlobal`
     font-family: sans-serif;
     background-color: ${colors.contrast};
   }
-`
+  .card{
+    border-radius: 5px;
+    background: #FFFFFF;
+    margin: 10px;
+    padding:0px;
+    height:auto;
+    -webkit-box-shadow: 0px 3px 5px -2px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0px 3px 5px -2px rgba(0,0,0,0.75);
+    box-shadow: 0px 3px 5px -2px rgba(0,0,0,0.75);
+  }
+  .card img{
+    margin-top:20px;
+    width:150px;
+    height:215px;
+  }
+  .card__description{
+    min-height:70px;
+  }
+  .card__title{
+    margin-bottom:0px;
+    font-size:16px;
+  }
+  .card__artist{
+    margin-bottom:0px;
+    margin-top:5px;
+    font-size:12px;
+  }
+`;
 
 // Simple helper function, takes in any number of props mapping to properties within the theme
 // object and returns the value.
-export const getTheme = (...props) => ({theme}) => props.reduce((t, p) => t[p], theme)
+export const getTheme = (...props) => ({ theme }) =>
+  props.reduce((t, p) => t[p], theme);
