5a204f3a9fc443f7ee2d1a599d6bcf2ca6c4ba7b
Sam Alghanmi <oalghnmi@gmail.com>
2018-09-12 08:30:36 -0400

    crud service

--- src/node/crud-service/crud-service.js
+++ src/node/crud-service/crud-service.js
@@ -1,3 +1,4 @@
+//you pass it the model, and it will just create crud endpoints?
 // basic route (http://localhost:8080)
 const express = require("express");
 
@@ -6,16 +7,9 @@ const express = require("express");
 // ---------------------------------------------------------
 var apiRoutes = express.Router();
 
-export default function auth({ app, Idea }) {
+export default function auth({ app, Model }) {
   apiRoutes.get("/", function(req, res) {
     res.send("Hello! Hello service is working");
   });
-  apiRoutes.get("/populate-db", function(req, res) {});
-  //sync with google: drive and bookmarks
-  //add idea
-  apiRoutes.post("idea",()=>{});
-  //if a specific idea title is specified, search all my knowledge for it?
-  apiRoutes.get("idea",(req,res)=>{});
-  apiRoutes.delete("idea",(req,res)=>{});
   return apiRoutes;
 }
