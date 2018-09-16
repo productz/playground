e0920ab816a84921b64aa4006c8e5dbb8fb5b35d
Sam Alghanmi <oalghnmi@gmail.com>
2018-09-15 23:12:15 -0400

    create a user page

--- src/react/App/App.js
+++ src/react/App/App.js
@@ -7,6 +7,8 @@ import { Crud } from "../crud-service/crud-service";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Home from "./Home";
+import User from "./User";
+import { ChatLog } from "./ChatLog";
 
 class App extends React.Component {
   render() {
@@ -48,47 +50,6 @@ class App extends React.Component {
 
 export const Admin = ({}) => {
   return <p>Admin Page</p>;
-};
-
-export const User = ({}) => {
-  return (
-    <Crud
-      modelName="user"
-      render={({ model, creatModel }) => {
-        let users = model;
-        if (users) {
-          let usersView = users.map(user => {
-            return (
-              <Grid xs={12} item>
-                {user.name}
-              </Grid>
-            );
-          });
-          return (
-            <Grid container>
-              <Paper>{usersView}</Paper>
-            </Grid>
-          );
-        }
-      }}
-    />
-  );
-};
-
-export const ChatLog = ({}) => {
-  return (
-    <Crud
-      modelName="chat-log"
-      render={({ model }) => {
-        let chatLogs = model;
-        if (chatLogs) {
-          return chatLogs.map(chatLog => {
-            return <p>chatLog.name</p>;
-          });
-        }
-      }}
-    />
-  );
 };
 
 ReactDOM.render(<App />, document.getElementById("app"));
