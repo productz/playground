94228101fdf16ea7165d1b6bae0701210b146542
Sam Alghanmi <oalghnmi@gmail.com>
2018-09-15 21:51:15 -0400

    sign in form

--- src/react/auth-service/MaterialLogin.js
+++ src/react/auth-service/MaterialLogin.js
@@ -1,28 +1,50 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Icon from "@material-ui/core/Icon";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle, faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import Avatar from "@material-ui/core/Avatar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import LockIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
 
-const styles = {
-  card: {
-    maxWidth: 345
+const styles = theme => ({
+  layout: {
+    width: "auto",
+    display: "block", // Fix IE11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
   },
-  media: {
-    height: 140
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
   }
-};
+});
 
 // Synchronous validation
 const LoginSchema = Yup.object().shape({
@@ -55,91 +77,99 @@ export const Login = ({
   classes
 }) => {
   return (
-    <Card className={classes.card}>
-      <CardHeader title="Login" />
-      <CardContent>
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          onSubmit={(values, actions) => {
-            onSubmit(values)
-              .then(() => {
-                actions.setSubmitting(false);
-              })
-              .catch(err => {
-
-              });
-          }}
-          validationSchema={LoginSchema}
-          render={({
-            values,
-            errors,
-            touched,
-            handleBlur,
-            handleChange,
-            handleSubmit,
-            isSubmitting
-          }) => {
-            return (
-              <form onSubmit={handleSubmit}>
-                {fields.map((field, index) => {
-                  return (
-                    <div key={index}>
-                      <TextField
-                        id={field.name}
-                        label={field.placeholder}
-                        type={field.type}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        maegin="normal"
-                        required={field.required}
-                        onKeyPress={event =>
-                          event.key === 13 ? handleSubmit(event) : ""
-                        }
-                      />
-                      {errors[field.name] &&
-                        touched[field.name] && <div>{errors[field.name]}</div>}
-                    </div>
-                  );
-                })}
-                <Button
-                  size="small"
-                  color="primary"
-                  onClick={onSubmit}
-                  type="submit"
-                  disabled={isSubmitting}
-                >
-                  Login
-                </Button>
-              </form>
-            );
-          }}
-        />
-      </CardContent>
-      <CardActions>
-        <Button size="small" color="secondary" onClick={onRegister}>
-          You don't have an account? register here
-        </Button>
-      </CardActions>
-      <div>
-        <Button onClick={() => onProviderAuth("google")} size="large" fullWidth>
-          Login with Google
-        </Button>
-        <Button
-          onClick={() => onProviderAuth("facebook")}
-          size="large"
-          fullWidth
-        >
-          Login with facebook
-        </Button>
-        <Button
-          onClick={() => onProviderAuth("twitter")}
-          size="large"
-          fullWidth
-        >
-          Login with twitter
-        </Button>
-      </div>
-    </Card>
+    <React.Fragment>
+      <CssBaseline />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockIcon />
+          </Avatar>
+          <Typography variant="headline">Sign in</Typography>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values, actions) => {
+              onSubmit(values)
+                .then(() => {
+                  actions.setSubmitting(false);
+                })
+                .catch(err => {});
+            }}
+            validationSchema={LoginSchema}
+            render={({
+              values,
+              errors,
+              touched,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              isSubmitting
+            }) => {
+              return (
+                <form onSubmit={handleSubmit}>
+                  {fields.map((field, index) => {
+                    return (
+                      <div key={index}>
+                        <TextField
+                          id={field.name}
+                          label={field.placeholder}
+                          type={field.type}
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          maegin="normal"
+                          required={field.required}
+                          onKeyPress={event =>
+                            event.key === 13 ? handleSubmit(event) : ""
+                          }
+                        />
+                        {errors[field.name] &&
+                          touched[field.name] && (
+                            <div>{errors[field.name]}</div>
+                          )}
+                      </div>
+                    );
+                  })}
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={onSubmit}
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Login
+                  </Button>
+                </form>
+              );
+            }}
+          />
+          <Button size="small" color="secondary" onClick={onRegister}>
+            You don't have an account? register here
+          </Button>
+          <div>
+            <Button
+              onClick={() => onProviderAuth("google")}
+              size="large"
+              fullWidth
+            >
+              Login with Google
+            </Button>
+            <Button
+              onClick={() => onProviderAuth("facebook")}
+              size="large"
+              fullWidth
+            >
+              Login with facebook
+            </Button>
+            <Button
+              onClick={() => onProviderAuth("twitter")}
+              size="large"
+              fullWidth
+            >
+              Login with twitter
+            </Button>
+          </div>
+        </Paper>
+      </main>
+    </React.Fragment>
   );
 };
 
