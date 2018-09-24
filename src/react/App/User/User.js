a41b7fd614dc364698086fd457999b8704aa44f7
Sam Alghanmi <oalghnmi@gmail.com>
2018-09-24 15:37:19 -0400

    incorporatd a search bar

--- src/react/App/User/User.js
+++ src/react/App/User/User.js
@@ -11,15 +11,73 @@ import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import Input from "@material-ui/core/Input";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { Route, Link } from "react-router-dom";
 import UserEdit from "./UserEdit";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import SearchIcon from "@material-ui/icons/Search";
 
 const styles = theme => ({
   root: {
+    width: "100%"
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginLeft: 0,
     width: "100%",
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing.unit,
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%"
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200
+      }
+    }
   }
 });
 
@@ -35,7 +93,8 @@ const User = ({
   editedModel,
   location,
   match,
-  history
+  history,
+  classes
 }) => {
   let users = model;
   if (users) {
@@ -67,9 +126,25 @@ const User = ({
       );
     });
     return (
-      <div>
+      <div className={classes.root}>
         <header>
-          <TextField />
+          <AppBar position="static">
+            <Toolbar>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <Input
+                  placeholder="Search…"
+                  disableUnderline
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                  }}
+                />
+              </div>
+            </Toolbar>
+          </AppBar>
         </header>
         <Route
           path={`${match.path}/:id`}
