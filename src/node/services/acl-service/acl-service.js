cefd2657fd9b3f3b0c421cc01e137852ce4ef247
Sam Alghanmi <oalghnmi@gmail.com>
2018-10-05 16:57:29 -0400

    Acl service

---
+++ src/node/services/acl-service/acl-service.js
@@ -0,0 +1,5 @@
+//check the database for something, pass it an acl Model (type here ...)
+//gets a domain logic -> checks if it's allowed in a database
+//the document in the database contains 
+//actions object
+//so that means we have to aggregate al domain objects into database
